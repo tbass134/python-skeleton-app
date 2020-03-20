c5531df11939703e9dcd6c2b9e698dd58a22d537
superdiana <diana.rodriguezmanrique@vonage.com>
2020-03-20 13:42:59 -0400

    additional instructions

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Simple Python Flask application to test and debug your Nexmo credentials and env
   - [API Credentials](#api-credentials)
   - [Using ngrok](#using-ngrok)
   - [Running the Application](#running-the-application)
+  - [Validate and Test your Credentials](#validate-and-test-your-credentials)
 - [Contributing](#contributing)
 - [License](#license)
 
@@ -37,7 +38,7 @@ Once you have downloaded a local copy, change into the directory of the applicat
 In order to test your API credentials, you need to init them first. From your terminal and inside the directory of the app just execute the **init.app.py** helper:
 
 ```bash
-python helper/init.app.py
+python helpers/init.app.py
 ```
 
 Then supply the required info, at the end of this process a **.env** will automatically be created in your root folder.
@@ -46,11 +47,29 @@ The `NEXMO_API_KEY` and `NEXMO_API_SECRET` are to be provided with your API key 
 
 As always, make sure to not commit your sensitive API credential data to any public version control. If you are using Git, you can add the `.env` file to your `.gitignore` file to ensure that it is not committed.
 
+### Validate and Test your credentials
+
+After starting your environment variables, you can validate credentials with the `nexmo.ops.py` helper. In fact, you can send an sms to your phone just to be sure that everything is working properly.
+
+So in your terminal, in the project directory execute:
+
+```bash
+python helpers/nexmo.ops.py
+```
+
+Then just select the option you want for testing.
+
 ### Using ngrok
 
 In order to test the incoming webhook data from Nexmo, the Nexmo API needs an externally accessible URL to send that data to. A commonly used service for development and testing is ngrok. The service will provide you with an externally available web address that creates a secure tunnel to your local environment. The [Nexmo Developer Platform](https://developer.nexmo.com/concepts/guides/testing-with-ngrok) has a guide to getting started with testing with ngrok.
 
-Once you have your ngrok URL, you can enter your [Nexmo Dashboard](https://dashboard.nexmo.com) and supply it as the `EVENT URL` for any Nexmo service that sends event data via a webhook. A good test case is creating a Voice application and providing the ngrok URL in the following format as the event url:
+Once you have your ngrok URL, you can enter your [Nexmo Dashboard](https://dashboard.nexmo.com/settings) and supply it as `DELIVERY RECEIPTS` for any Nexmo service that sends data related with the message delivery via webhook. The Url format is similar to:
+
+`#{ngrok URL}/webhooks/delivery`
+
+Then using the endpoint `#{ngrok URL}/send-sms`, you can simulate sending an sms to your mobile and check the server webhook response in the **flask logs**
+
+Another test case is creating a Voice application and providing the ngrok URL in the following format as the event url:
 
 `#{ngrok URL}/webhooks/event`
 
@@ -61,13 +80,25 @@ You can then call your Nexmo Voice application, and with your skeleton applicati
 Once you have your API credentials incorporated and your ngrok setup ready, you can go ahead and use this skeleton app. To start the application's server, run the following from the command line inside the directory of the app:
 
 ```bash
-python app.py
+export FLASK_APP=app.py && python -m flask run
 ```
+
+You can test your credentials or send an sms using the cli helper, but you can do it using the next **web application endpoints**:
+
+`#{ngrok URL}/check-credentials`
+
+`#{ngrok URL}/send-sms`
 
 The skeleton app is also capable of receiving Nexmo API webhook data. As mentioned in the [Using ngrok](#using-ngrok) section above, a good candidate for that test is a Nexmo Voice application. From within your Nexmo dashboard you can create a Nexmo Voice application, provision a Nexmo virtual phone number and then link that number to your Voice application. Once you have ensured that your new Voice application's `EVENT URL` is `#{ngrok URL}/webhooks/event`, you can then give your Nexmo number a phone call. You should see the webhook data in your console in real time. For example, data for a ringing phone call will look like this:
 
-```
+```json
 {:from=>"447700900000", :to=>"447700900000", :uuid=>"a123456789012345fbdsw", :conversation_uuid=>"CON-234567-fdsfs34-vfddfh-btger3-22345", :status=>"ringing", :direction=>"inbound", :timestamp=>"2020-01-07T11:24:49.478Z"}
+```
+
+For **SMS delivery endpoint** `#{ngrok URL}/webhooks/delivery` the output should be something like this: (Real details not shown. Obscured as: "[example]")
+
+```python
+ImmutableMultiDict([('msisdn', '[Your Number]'), ('to', '[Nexmo Number]'), ('network-code', '[XXXXX]'), ('messageId', '[Message ID]'), ('price', '[price]'), ('status', 'delivered'), ('scts', '[XXXXXXXX]'), ('err-code', '0'), ('api-key', '[Your API Key]'), ('message-timestamp', '2020-03-19 20:20:42')])
 ```
 
 You can exit your application at anytime by holding down the CTRL and C keys on your keyboard.
--- env.py
+++ env.py
@@ -1,48 +1,55 @@
-import os, sys
+import os
+import sys
 from os.path import join, dirname
 from dotenv import load_dotenv
+
 
 class env:
     def __init__(self, env_file='.env'):
         self.params = []
         self.file = env_file
         self.is_loaded = False
-        #If file exist then load it
+        # If file exist then load it
         if self.exists():
             self.load()
             self.is_loaded = True
 
-    #If default value is None, then param is required. If default value is present then is optional
-    def add_param(self, name, value = None):
-        #check if param exists already
-        indices = [index for index, param in enumerate(self.params) if param['name'] == name]
-        if len(indices) == 0: 
-            self.params.append({"name":name, "value": value})
+    # If default value is None, then param is required. If default value is present then is optional
+    def add_param(self, name, value=None):
+        # check if param exists already
+        indices = [index for index, param in enumerate(
+            self.params) if param['name'] == name]
+        if len(indices) == 0:
+            self.params.append({"name": name, "value": value})
         else:
-            #If exist and default value not none, then replace the value of existing param
+            # If exist and default value not none, then replace the value of existing param
             #print("Error: The param already exist")
             index = indices[0]
             if value is not None:
-                self.params[index]["value"] = value 
+                self.params[index]["value"] = value
+
     def exists(self):
         if not os.path.isfile(self.file):
             return False
         else:
             return True
+
     def create(self):
         error = False
         try:
             env = open(self.file, "a")
             print("Creating your {} file\n".format(self.file))
             for index, param in enumerate(self.params):
-                option = input(param["name"] + (" ( " + param["value"] + " )" if param["value"] is not None else "") + " : ")
+                option = input(param["name"] + (" ( " + param["value"] +
+                                                " )" if param["value"] is not None else "") + " : ")
                 if param["value"] is None and option == '':
                     raise Exception("invalid value to param")
                 elif option == '':
                     option = param["value"]
-                env.write("{2}{0}=\"{1}\"".format(param["name"], option, "\n" if index != 0 else ""))
+                env.write("{2}{0}=\"{1}\"".format(
+                    param["name"], option, "\n" if index != 0 else ""))
         except OSError:
-            print ("Could not open/read file: ", self.file)
+            print("Could not open/read file: ", self.file)
             error = True
         except IOError:
             print("An error ocurred trying to write file: ", self.file)
@@ -55,10 +62,11 @@ class env:
             if error:
                 os.remove(self.file)
                 sys.exit()
+
     def update(self):
-        #Clean file content
-        open(self.file,"w").close()
-        #recreate file content
+        # Clean file content
+        open(self.file, "w").close()
+        # recreate file content
         error = False
         try:
             env = open(self.file, "a")
@@ -67,9 +75,10 @@ class env:
                 if param["value"] is None:
                     raise Exception("invalid value to param")
                 else:
-                    env.write("{2}{0}=\"{1}\"".format(param["name"], param["value"], "\n" if index != 0 else ""))
+                    env.write("{2}{0}=\"{1}\"".format(
+                        param["name"], param["value"], "\n" if index != 0 else ""))
         except OSError:
-            print ("Could not open/read file: ", self.file)
+            print("Could not open/read file: ", self.file)
             error = True
         except IOError:
             print("An error ocurred trying to write file: ", self.file)
@@ -82,29 +91,33 @@ class env:
             if error:
                 sys.exit()
             else:
-                #Load the file
+                # Load the file
                 self.load()
+
     def load(self):
-        #Load environment
-        envpath = join(dirname(__file__),"./"+self.file)
+        # Load environment
+        envpath = join(dirname(__file__), "./"+self.file)
         load_dotenv(envpath)
-        #Get the current values - Usefull for update process
+        # Get the current values - Useful for update process
         try:
             env_lines = open(self.file, "r")
             for line in env_lines:
-                clean_line = line.replace('"','')
+                clean_line = line.replace('"', '')
                 if len(clean_line.split("=")) == 2:
-                    (name,value) = list(clean_line.split("="))
+                    (name, value) = list(clean_line.split("="))
                     self.add_param(name=name, value=value)
         except:
             print("There was an error trying to extract the "+self.file+" values")
         finally:
             env_lines.close()
+
     def set_param(self, name, value):
         self.add_param(name, value)
+
     def get(self, name):
         return self.params[name]
-    def get_env_vars(self, reload = False):
+
+    def get_env_vars(self, reload=False):
         if not self.is_loaded or reload:
             os.environ.clear()
             self.load()
